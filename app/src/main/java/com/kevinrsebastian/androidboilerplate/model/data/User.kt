b45328b367286eb11e4f93d2e3a6eea2534bc05e
Kevin Sebastian <kevinrsebastian@gmail.com>
2022-01-29 12:33:30 +0800

    Enable Parcelize annotation and apply to the User data class

--- app/src/main/java/com/kevinrsebastian/androidboilerplate/model/data/User.kt
+++ app/src/main/java/com/kevinrsebastian/androidboilerplate/model/data/User.kt
@@ -1,7 +1,11 @@
 package com.kevinrsebastian.androidboilerplate.model.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     val id: String,
     val firstName: String,
     val lastName: String
-)
+) : Parcelable
