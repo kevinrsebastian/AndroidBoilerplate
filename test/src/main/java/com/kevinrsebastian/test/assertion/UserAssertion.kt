7744c168f009495a0a0bd784217590f465489257
Kevin Sebastian <kevinrsebastian@gmail.com>
2022-04-03 23:45:37 +0800

    Fix UserAssertion comparing the same instance instead of another

--- test/src/main/java/com/kevinrsebastian/test/assertion/UserAssertion.kt
+++ test/src/main/java/com/kevinrsebastian/test/assertion/UserAssertion.kt
@@ -6,9 +6,9 @@ import org.assertj.core.api.Assertions.assertThat
 object UserAssertion {
 
     fun assertEqual(user1: User, user2: User) {
-        assertThat(user1.id).isEqualTo(user1.id)
-        assertThat(user1.firstName).isEqualTo(user1.firstName)
-        assertThat(user1.lastName).isEqualTo(user1.lastName)
+        assertThat(user1.id).isEqualTo(user2.id)
+        assertThat(user1.firstName).isEqualTo(user2.firstName)
+        assertThat(user1.lastName).isEqualTo(user2.lastName)
     }
 
     fun assertEqual(users1: List<User>, users2: List<User>) {
